29956bfcfed4dc5f9cf4ab8d03330725b3f5097b
Sagar Singha Roy <sagarsroy@outlook.com>
2021-02-15 17:20:43 +0530

    1.Gomati Section Added 2.Organice Section Added 3.Exoctic Section Added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,58 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
   title = 'demo1';
+
+  data = [
+    {
+      img: '../assets/Gomoti Products/Chomchum.JPG',
+      name: 'chomchom',
+      rupee: 200,
+      desc: 'Some text about the jeans. Super slim and comfy lorem ipsum lorem jeansum. Lorem jeamsun denim lorem jeansum.'
+    },
+    {
+      img: '../assets/Gomoti Products/Chomchum.JPG',
+      name: 'chomchom',
+      rupee: 200,
+      desc: 'Some text about the jeans. Super slim and comfy lorem ipsum lorem jeansum. Lorem jeamsun denim lorem jeansum.'
+    },
+    {
+      img: '../assets/Gomoti Products/Chomchum.JPG',
+      name: 'chomchom',
+      rupee: 200,
+      desc: 'Some text about the jeans. Super slim and comfy lorem ipsum lorem jeansum. Lorem jeamsun denim lorem jeansum.'
+    },
+    {
+      img: '../assets/Gomoti Products/Chomchum.JPG',
+      name: 'chomchom',
+      rupee: 200,
+      desc: 'Some text about the jeans. Super slim and comfy lorem ipsum lorem jeansum. Lorem jeamsun denim lorem jeansum.'
+    },
+    {
+      img: '../assets/Gomoti Products/Chomchum.JPG',
+      name: 'chomchom',
+      rupee: 200,
+      desc: 'Some text about the jeans. Super slim and comfy lorem ipsum lorem jeansum. Lorem jeamsun denim lorem jeansum.'
+    },
+    {
+      img: '../assets/Gomoti Products/Chomchum.JPG',
+      name: 'chomchom',
+      rupee: 200,
+      desc: 'Some text about the jeans. Super slim and comfy lorem ipsum lorem jeansum. Lorem jeamsun denim lorem jeansum.'
+    },
+    {
+      img: '../assets/Gomoti Products/Chomchum.JPG',
+      name: 'chomchom',
+      rupee: 200,
+      desc: 'Some text about the jeans. Super slim and comfy lorem ipsum lorem jeansum. Lorem jeamsun denim lorem jeansum.'
+    },
+    {
+      img: '../assets/Gomoti Products/Chomchum.JPG',
+      name: 'chomchom',
+      rupee: 200,
+      desc: 'Some text about the jeans. Super slim and comfy lorem ipsum lorem jeansum. Lorem jeamsun denim lorem jeansum.'
+    }
+  ]
 }
 
